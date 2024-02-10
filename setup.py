802948f5aaaa671254fb936ff9df85a4e959ea78
Yue Zhao <zhao13@ualberta.ca>
2024-02-10 03:56:40 +0000

    Added python version requirement

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+PACKAGE_NAME = 'gaia_cmd_plotter'
+PACKAGE_VERSION = '0.1'
+AUTHOR_NAME = 'Yue Zhao'
+AUTHOR_EMAIL = 'Yue.Zhao@soton.ac.uk'
+
 setup(
-    name='gaia_cmd_plotter',
-    version='0.1',
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    python_requires='>=3.8',
     packages=find_packages(),
     install_requires=[
         'matplotlib>=3.3.0',
@@ -10,8 +16,8 @@ setup(
     package_data={
         'gaia_cmd_plotter': ['gaia_cmd.mplstyle', 'data/gaia_cmd_background.png'],
     },
-    author='Yue Cory Zhao',
-    author_email='Yue.Zhao@soton.ac.uk',
+    author=AUTHOR_NAME,
+    author_email=AUTHOR_EMAIL,
     description='A Python package that provides a matplotlib.pyplot.Axes object that displays a Gaia CMD background.',
     url='https://github.com/coryzh/gaia_cmd_plotter',
 )
